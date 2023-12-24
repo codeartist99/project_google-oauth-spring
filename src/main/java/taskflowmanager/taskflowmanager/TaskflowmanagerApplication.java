d8951ced0f81d8a89a177482b5f42eafca670586
codeartitect <ysw991106@gmail.com>
2023-12-24 17:12:13 +0900

    first commit - project init

---
+++ src/main/java/taskflowmanager/taskflowmanager/TaskflowmanagerApplication.java
@@ -0,0 +1,13 @@
+package taskflowmanager.taskflowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowmanagerApplication.class, args);
+	}
+
+}
