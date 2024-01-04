555acba4adf15b05cae741538e95d562218194df
codeartitect <ysw991106@gmail.com>
2024-01-04 18:45:22 +0900

    CustomUserDetails 파라미터 값 수정

--- src/main/java/taskflowmanager/taskflowmanager/security/CustomUserDetailsService.java
+++ src/main/java/taskflowmanager/taskflowmanager/security/CustomUserDetailsService.java
@@ -1,4 +1,10 @@
 package taskflowmanager.taskflowmanager.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface CustomUserDetailsService {
+
+    UserDetails loadUserByUsername(String email);
+
+    UserDetails loadUserbyId(Long id);
 }
--- src/main/java/taskflowmanager/taskflowmanager/security/CutomUserDetailsServiceImpl.java
+++ src/main/java/taskflowmanager/taskflowmanager/security/CutomUserDetailsServiceImpl.java
@@ -1,2 +1,37 @@
-package taskflowmanager.taskflowmanager.security;public class CutomUserDetailsServiceImpl {
+package taskflowmanager.taskflowmanager.security;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import taskflowmanager.taskflowmanager.exception.ResourceNotFoundException;
+import taskflowmanager.taskflowmanager.model.User;
+import taskflowmanager.taskflowmanager.repository.UserRepository;
+
+@Service
+public class CutomUserDetailsServiceImpl implements UserDetailsService, CustomUserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                new UsernameNotFoundException("User not found with email : " + email));
+        return UserPrincipal.create(user);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserbyId(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", id)
+        );
+        return UserPrincipal.create(user);
+    }
+
 }
