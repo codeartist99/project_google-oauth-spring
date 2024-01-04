4d50a793d5c4b008e0aaf0411fc0409da7933b28
codeartitect <ysw991106@gmail.com>
2024-01-04 16:57:39 +0900

    google user info 생성

--- src/main/java/taskflowmanager/taskflowmanager/security/oauth2/user/OAuth2UserInfo.java
+++ src/main/java/taskflowmanager/taskflowmanager/security/oauth2/user/OAuth2UserInfo.java
@@ -3,8 +3,12 @@ package taskflowmanager.taskflowmanager.security.oauth2.user;
 import lombok.Data;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public abstract class OAuth2UserInfo {
+
+    protected Map<String, Object> attributes;
 
     private String email;
 
@@ -14,4 +18,7 @@ public abstract class OAuth2UserInfo {
 
     private String ImageUrl;
 
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
 }
--- src/main/java/taskflowmanager/taskflowmanager/security/oauth2/user/OAuth2UserInfoFactory.java
+++ src/main/java/taskflowmanager/taskflowmanager/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -1,8 +1,13 @@
 package taskflowmanager.taskflowmanager.security.oauth2.user;
 
+import taskflowmanager.taskflowmanager.model.AuthProvider;
+
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        }
     }
 }
